b935b80c122b401b43395609739db50ea0ec4536
Esperaanza <xcaanda100@gmail.com>
2023-01-24 09:18:20 -0600

    new file:   package-lock.json 	modified:   package.json 	modified:   src/index.html 	modified:   src/style.css 	modified:   src/validator.js

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,9 @@
-const validator = {
-  // ...
+function validator()
+ {
+  const validator=document.querySelector('.num')
+  validator.addEventListener('click',()=>{
+  console.log ('hola');
+  });
 };
 
 export default validator;
