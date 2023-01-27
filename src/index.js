f5f9a5703097361a02f1d2941e4fd1ae59b83ce2
Esperaanza <xcaanda100@gmail.com>
2023-01-26 22:09:43 -0600

    casi

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import validator from './validator.js';
+ 
+document.getElementById("btn").addEventListener('click',()=>{
+    // hay que buscar el valor del in´put 
 
-console.log(validator);
+const value =  document.getElementById('cardnumber').value
+const creditcardNumber =document.getElementById('cardnumber').value
+document.getElementById("cardnumber").value= enmascarar(value);
+function enmascarar(creditcardNumber) {
+ 
+ return validator.maskify(creditcardNumber);
+}
+    validator.isvalid(value)
+    })
+//document.getElementById("submit").addEventListener('click',validar,true);
+
--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,24 @@
-function validator()
- {
-  const validator=document.querySelector('.num')
-  validator.addEventListener('click',()=>{
-  console.log ('hola');
-  });
-};
+/*Se hizo la constante validador que contiene los metodos 
+de enmascarar y validar el numero de tarjeta*/
+const validator={
+  isvalid :function(){
+  console.log("yo soy el Metodo")
+  
+
+  },
+  maskify :function() {
+      var cardnumber=document.getElementById('cardnumber').value
+      var numeral = "#"; //se define el simbolo con q se enmascara los digitos de la tarjeta
+      var tarjetaEnmascarada = cardnumber.slice(-4) //Se obtine los ultimos 4 digitos de la tarjeta
+      if (cardnumber.length > 4 ){ //si el largo de la tarjeta es mayor a 4 se enmascara
+          /*Repite el numero de la tarjeta enmascardo exepto los ultimos 4*/
+          tarjetaEnmascarada = numeral.repeat(cardnumber.length-4) + cardnumber.slice(-4) 
+      }
+      return tarjetaEnmascarada;
+  
+    
+ } 
+}
+
 
 export default validator;
